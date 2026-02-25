ae3f372b535811766da383fc35180c7e77730322
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-25 17:09:27 -0300

    feat: roteamente das páginas

---
+++ src/router.tsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import BookDetails from "./pages/Book-Details";
+import GenrePage from "./pages/Genre-Page";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <RootLayout/>,
+        children: [
+            {
+                index: true,
+                element: <Login/>
+            },
+            {
+                path: '/home',
+                element: <Home/>
+            },
+            {
+                path: '/BookDetails',
+                element: <BookDetails/>
+            },
+            {
+                path: '/GenrePage',
+                element: <GenrePage/>
+            }
+        ]
+    }
+])
+
+export default router
