ea391b416d0736868d91bbb68e49e675586ecec2
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-25 21:55:33 -0300

    feat: add página de login funcional

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,23 @@
+import Form from "../../components/Forms";
+import styles from "./styles.module.css";
+import Logo from "../../assets/Logo.png";
+
 export default function Login() {
   return (
     <>
-      <h1>Login</h1>
+      <div className={styles.container}>
+        <figure className={styles.figure}/>
+        <div className={styles.info}>
+          <figure className={styles.Logo}>
+            <img src={Logo} />
+          </figure>
+          <section className={styles.text}>
+            <h2>Bem vindo(a)!</h2>
+            <h1>Entre na sua conta</h1>
+          </section>
+          <Form />
+        </div>
+      </div>
     </>
   );
 }
