c4d2f6cefab19c61b7dad6abbe1a77bc4da6ecc1
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-26 20:06:17 -0300

    feat: rotas personalizadas no router

--- src/router.tsx
+++ src/router.tsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
                 element: <Home/>
             },
             {
-                path: '/BookDetails',
+                path: '/BookDetails/:bookID',
                 element: <BookDetails/>
             },
             {
