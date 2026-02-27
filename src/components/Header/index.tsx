6e140803a6ae6669c89262cbeb59110616f0410f
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-26 22:01:19 -0300

    feat: add funcionalidade do botão que leva pra home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,16 +2,17 @@ import Logo from "../../assets/Logo.png";
 import LoginIcon from "../../assets/LoginIcon.png";
 import CartIcon from "../../assets/cartIcon.png";
 import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.mainButton}>
-        <button>
+        <Link to='/home'>
           <figure>
             <img src={Logo} />
           </figure>
-        </button>
+        </Link>
       </div>
       <div className={styles.buttons}>
         <button>
