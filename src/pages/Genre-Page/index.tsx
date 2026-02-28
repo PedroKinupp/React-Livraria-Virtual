cbc9e49698b25ca9f429f1a8c01dbb99519ddc36
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-27 22:47:22 -0300

    feat: add funcionalidade de pesquisa para a página de cada genero literário

--- src/pages/Genre-Page/index.tsx
+++ src/pages/Genre-Page/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BookService } from "../../services/BookService";
 import type { BookType } from "../../types/BookType";
+import searchIcon from "../../assets/searchIcon.png";
+import backIcon from "../../assets/backIcon.png";
 import Header from "../../components/Header";
 import BookCard from "../../components/BookCard";
 import styles from "./styles.module.css";
@@ -9,38 +11,54 @@ import styles from "./styles.module.css";
 export default function GenrePage() {
   const { Genre } = useParams();
   const [books, setBook] = useState<BookType[]>([]);
+  const [search, setSearch] = useState<string>("");
 
-  async function loadBook(Genre: string | undefined) {
+  async function loadBook(Genre: string | undefined, search: string) {
     if (Genre) {
-      const response = await BookService.getByGenre(Genre, "");
+      const response = await BookService.getByGenre(Genre, search);
       setBook(response);
     }
   }
 
   useEffect(() => {
-    loadBook(Genre);
-  }, []);
+    loadBook(Genre, search);
+  }, [Genre, search]);
   return (
     <>
       <Header />
-      <h1>Pagina dos Genero: {Genre}</h1>
       <div className={styles.container}>
-        {books.length > 0 ? (
-          books?.map((book) => (
-            <ul key={book.id}>
-              <BookCard
-                autor={book.autor}
-                capa={book.capa}
-                preco={book.preco}
-                titulo={book.titulo}
-                isCompact={false}
-              />
-              <br />
-            </ul>
-          ))
-        ) : (
-          <h1 className={styles.errorMessage}>Genero não encontrado :(</h1>
-        )}
+        <div className={styles.searchBox}>
+          <img src={searchIcon} />
+          <input
+            className={styles.input}
+            placeholder="Pesquisar por título"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+        <div className={styles.main}>
+          <button className={styles.button}>
+            <img src={backIcon} />
+            {Genre}
+          </button>
+          <div className={styles.box}>
+            {books.length > 0 ? (
+              books?.map((book) => (
+                <ul key={book.id}>
+                  <BookCard
+                    autor={book.autor}
+                    capa={book.capa}
+                    preco={book.preco}
+                    titulo={book.titulo}
+                    isCompact={false}
+                  />
+                  <br />
+                </ul>
+              ))
+            ) : (
+              <h1 className={styles.errorMessage}>Livro não encontrado :(</h1>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
