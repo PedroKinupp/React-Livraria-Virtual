f5afc02b109783cd75d83fd7206344d929afff73
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-02-28 18:14:44 -0300

    feat: página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,21 @@
+import BookShelf from "../../components/BookShelf";
+import Header from "../../components/Header";
+import styles from './styles.module.css'
+import BannerDiscount from '../../assets/BannerDiscount.png'
+
+
 export default function Home() {
+
+
   return (
     <>
-      <h1>Home</h1>
+      <Header />
+      <div className={styles.container}>
+        <figure className={styles.discount}>
+          <img src={BannerDiscount} className={styles.discountImg}/>
+        </figure>
+        <BookShelf/>
+      </div>
     </>
   );
 }
