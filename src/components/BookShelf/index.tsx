3c87b926b164ed29730d1895f4b94db5347c0ca2
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-06 23:36:24 -0300

    refactor: refatorando a antiga bookshelf

--- src/components/BookShelf/index.tsx
+++ src/components/BookShelf/index.tsx
@@ -1,9 +1,5 @@
-import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
-import type { BookType } from "../../types/BookType";
-import { BookService } from "../../services/BookService";
-import BookCard from "../BookCard";
-import { Link } from "react-router-dom";
+import Shelf from "../Shelf";
 
 const Generos = [
   "Best-sellers",
@@ -16,43 +12,11 @@ const Generos = [
 ];
 
 export default function BookShelf() {
-  const [books, setBook] = useState<BookType[][]>([]);
-
-  async function loadBooks() {
-    const responses = await Promise.all(
-      Generos.map((element) => BookService.getByGenre(element, "", 4)),
-    );
-
-    setBook(responses);
-  }
-
-  useEffect(() => {
-    loadBooks();
-  }, []);
 
   return (
-    <div className={styles.bookShelf}>
-      {books.map((bookByGenre, index) => (
-        <section key={index} className={styles.shelf}>
-            <div className={styles.header}>
-                <h1>{Generos[index]}</h1>
-                <Link to={`/GenrePage/${Generos[index]}`} className={styles.link}>Ver mais</Link>
-            </div>
-            <div className={styles.booksByGenre}>
-            {bookByGenre.map((book) => (
-                <ul key={book.id}>
-                <BookCard
-                    autor={book.autor}
-                    capa={book.capa}
-                    preco={book.preco}
-                    titulo={book.titulo}
-                    isCompact={true}
-                    id={book.id}
-                />
-                </ul>
-            ))}
-            </div>
-        </section>
+    <div className={styles.BookShelf}>
+      {Generos.map(element =>(
+        <Shelf key={element} genre={element}/>
       ))}
     </div>
   );
