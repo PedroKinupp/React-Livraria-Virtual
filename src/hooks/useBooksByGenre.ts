6a1f97349eb87fc541f1a74148dc824b517e51ee
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-06 23:37:09 -0300

    feat: add limite de livros (by genero) por requisição no hook

--- src/hooks/useBooksByGenre.ts
+++ src/hooks/useBooksByGenre.ts
@@ -4,15 +4,20 @@ import type { BookResponse } from "../types/BookType";
 
 const API_URL = "http://localhost:3000"
 
-const BooksByGenre = async (genre: string): AxiosPromise<BookResponse> => {
-    const response = await axios.get<BookResponse>(API_URL + `/livros/?genero=${genre}`);
-    return response;
+const BooksByGenre = async (genre: string, limit?: number): AxiosPromise<BookResponse> => {
+    if(limit){
+        const response = await axios.get<BookResponse>(API_URL + `/livros/?genero=${genre}&_limit=${limit}`);
+        return response;
+    }else{
+        const response = await axios.get<BookResponse>(API_URL + `/livros/?genero=${genre}`);
+        return response;
+    }
 }
 
-export function useBooksByGenre(genre?:string){
+export function useBooksByGenre(genre?:string, limit?: number){
     const query = useQuery({
-        queryFn: () => BooksByGenre(genre || ''),
-        queryKey: ['books']
+        queryFn: () => BooksByGenre(genre || '', limit),
+        queryKey: ['books', genre]
     })
 
     return {
