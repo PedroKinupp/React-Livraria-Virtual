265e07327592e1eef3a816a6cb5d019058236b36
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-06 23:37:25 -0300

    chore: comentando as antigas requisições

--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,26 +1,26 @@
-import axios from "axios"
-import type { BookType } from "../types/BookType"
+// import axios from "axios"
+// import type { BookType } from "../types/BookType"
 
-const api = axios.create({
-  baseURL: "http://localhost:3000"
-})
+// const api = axios.create({
+//   baseURL: "http://localhost:3000"
+// })
 
-export const BookService = {
+// export const BookService = {
 
-  async getByID(id: number): Promise<BookType> {
-    const response = await api.get<BookType>(`/livros/${id}`)
-    return response.data
-  },
+//   async getByID(id: number): Promise<BookType> {
+//     const response = await api.get<BookType>(`/livros/${id}`)
+//     return response.data
+//   },
 
-  async getByGenre(genero: string, titulo?: string, limit?: number): Promise<BookType[]> {
-    const response = await api.get<BookType[]>("/livros", {
-      params: { 
-        genero,
-        titulo_like : titulo,
-        _limit: limit
-      }
-    })
-    return response.data
-  }
+//   async getByGenre(genero: string, titulo?: string, limit?: number): Promise<BookType[]> {
+//     const response = await api.get<BookType[]>("/livros", {
+//       params: { 
+//         genero,
+//         titulo_like : titulo,
+//         _limit: limit
+//       }
+//     })
+//     return response.data
+//   }
 
-}
+// }
