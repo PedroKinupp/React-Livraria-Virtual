5e7ae7c4452cd0e01167c163fd602a6706e303d2
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-07 21:07:33 -0300

    feat: add mensagem de carregando e erro para detalhes do livro

--- src/pages/Book-Details/index.tsx
+++ src/pages/Book-Details/index.tsx
@@ -3,11 +3,13 @@ import Header from "../../components/Header";
 import styles from "./styles.module.css";
 import back from "../../assets/backIcon.png";
 import { useBooksByID } from "../../hooks/useBooksByID";
+import LoadingMsg from "../../components/loadingMsg";
+import ErrorMsg from "../../components/errorMsg";
 
 export default function BookDetails() {
   const navigate = useNavigate()
   const { bookID } = useParams();
-  const { data } = useBooksByID(Number(bookID));
+  const { data, isLoading, isError, isSuccess } = useBooksByID(Number(bookID));
 
   return (
     <>
@@ -15,28 +17,43 @@ export default function BookDetails() {
       <main>
         <div className={styles.box}>
           <button onClick={() => navigate(-1)}className={styles.goBack}>
-            <img src={back} />
+            <img src={back}/>
             Detalhes do Livro
           </button>
-          <div className={styles.container}>
-            <figure>
-              <img src={data?.capa} />
-            </figure>
-            <div className={styles.info}>
-              <section className={styles.header}>
-                <h1>{data?.titulo}</h1>
-                <h2>{data?.autor}</h2>
-              </section>
-              <article className={styles.article}>
-                <h3>Sinopse</h3>
-                <p>{data?.sinopse}</p>
-              </article>
+
+          {isLoading ? 
+            <div className={styles.msg}>
+              <LoadingMsg/>
+            </div> : <></>}
+
+          {isError ? 
+            <div className={styles.msg}>
+              <ErrorMsg/>
+            </div> : <></>}
+
+          {isSuccess ? 
+            <>
+              <div className={styles.container}>
+              <figure>
+                <img src={data?.capa} />
+              </figure>
+              <div className={styles.info}>
+                <section className={styles.header}>
+                  <h1>{data?.titulo}</h1>
+                  <h2>{data?.autor}</h2>
+                </section>
+                <article className={styles.article}>
+                  <h3>Sinopse</h3>
+                  <p>{data?.sinopse}</p>
+                </article>
+              </div>
             </div>
-          </div>
-          <button className={styles.addCart}>
-            <span>R$ {data?.preco}</span>
-            <span>Adicionar ao carrinho</span>
-          </button>
+            <button className={styles.addCart}>
+              <span>R$ {data?.preco}</span>
+              <span>Adicionar ao carrinho</span>
+            </button>
+            </> : <></>
+          }
         </div>
       </main>
     </>
